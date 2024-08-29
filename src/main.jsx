b45a3f071816fb12dce7423db2f1d5036a7e5d99
pomo heri <pomoheri@gmail.com>
2024-08-29 15:23:29 +0700

    initial project

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+//import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+//import BrowserRouter dari react router
+import { BrowserRouter } from 'react-router-dom';
+
+import { AuthProvider } from './context/AuthContext.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
+)
